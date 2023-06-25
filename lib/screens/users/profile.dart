bfbf90d23f1ebd10178550307f4c79d59e67a832
e.yunwen <e.yunwen@commonfactor.tech>
2023-06-25 11:50:08 +0100

    added the ui for kyc for users

--- lib/screens/users/profile.dart
+++ lib/screens/users/profile.dart
@@ -10,6 +10,8 @@ import 'package:zappy_meal/models/user/user_model.dart';
 import 'package:zappy_meal/routes/index.dart';
 import 'package:zappy_meal/screens/users/widget.dart';
 import 'package:zappy_meal/shared/components/avatar_circle.dart';
+import 'package:zappy_meal/shared/components/bottom_sheets.dart';
+import 'package:zappy_meal/shared/components/buttons.dart';
 import 'package:zappy_meal/shared/components/modals.dart';
 import 'package:zappy_meal/shared/components/radius.dart';
 import 'package:zappy_meal/shared/components/shadow.dart';
@@ -149,15 +151,34 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               children: [
                                 profileInfo(context: context, title: "Reset Account", value: "", icon: AppIcons.user, scale: 0.8),
                                 profileInfo(
-                                  onTap: () => AppModal.showInfoAlert(
+                                  onTap: () => AppBottomSheet.simpleSheet(
+                                    alignment: Alignment.center,
                                     context: context,
-                                    desc: "Are you sure you want to logout?",
-                                    title: "Log out!",
-                                    onCancel: () {},
-                                    btnOkOnPress: () async {
-                                      await LocalPrefs.logOutUser();
-                                      context.go(AppRoutes.splash);
-                                    },
+                                    height: 300.h,
+                                    padding: kpadding(20.w, 30.h),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text("Are you sure you want to log out?", style: Theme.of(context).textTheme.displayMedium, textAlign: TextAlign.center),
+                                        kh20Spacer(),
+                                        submitButton(
+                                          width: 200.w,
+                                          context: context,
+                                          onPressed: () async {
+                                            await LocalPrefs.saveToken("");
+                                            context.go(AppRoutes.splash);
+                                          },
+                                          text: "Yes",
+                                        ),
+                                        kh10Spacer(),
+                                        submitButton(
+                                          width: 200.w,
+                                          context: context,
+                                          onPressed: () {},
+                                          text: "No",
+                                        )
+                                      ],
+                                    ),
                                   ),
                                   context: context,
                                   title: "Logout",
